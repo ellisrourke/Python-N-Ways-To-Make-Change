6a657bfbc649e106e12ffd9fa9d98d98a675687d
ellisrourke <ellisrourke@gmail.com>
2020-03-06 12:52:27 +1000

    recur

--- recursion.py
+++ recursion.py
@@ -1,28 +1,24 @@
 
-def coinChange(coins, remainder, cache):
-    if(remainder < 0):
-        return -1
+class coinRecursion():
+    coins = [1, 2]
+    amount = 4
 
-    if(remainder == 0):
-        return 0
-
-    if(cache[remainder]!=0):
-        return cache[remainder]
-
-    min = 0
-    for coin in coins:
-        change = coinChange(coins,remainder-coin,cache)
-
-        if(change >= 0 and change < min):
-            min = 1 + change
-
-        #???
-        min = -1
-
-        return cache[remainder]
+    def __init__(self):
+        print(self.coinChange(self.amount,self.coins))
 
 
-coins = [1,2,3]
-amount = 5
-cache = [0]*(amount+1)
-coinChange(coins,amount,cache)
+    def coinChange(self,amount,coins):
+        if(amount == 0):
+            return 1
+        if(amount < 0):
+            return 0
+
+        combinations = 0
+
+        #for coin in self.coins:
+        for i in range(0,len(self.coins)):
+            combinations += self.coinChange(amount - coins[i],coins)
+
+        return combinations
+
+solve = coinRecursion()
