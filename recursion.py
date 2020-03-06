99ac404564ec55663231a7f6fc6d753a8656b4b1
ellisrourke <ellisrourke@gmail.com>
2020-03-06 13:02:40 +1000

    recur

--- recursion.py
+++ recursion.py
@@ -2,12 +2,13 @@
 class coinRecursion():
     coins = [1, 2]
     amount = 4
-
+    currentCoin = coins[0]
     def __init__(self):
-        print(self.coinChange(self.amount,self.coins))
+        print(self.coinChange(self.amount,self.coins,self.currentCoin))
 
 
-    def coinChange(self,amount,coins):
+    def coinChange(self,amount,coins,currentCoin):
+        print(currentCoin)
         if(amount == 0):
             return 1
         if(amount < 0):
@@ -16,8 +17,9 @@ class coinRecursion():
         combinations = 0
 
         #for coin in self.coins:
-        for i in range(0,len(self.coins)):
-            combinations += self.coinChange(amount - coins[i],coins)
+        for i in range(currentCoin,len(self.coins)):
+            combinations += self.coinChange(amount - coins[i],coins,i)
+
 
         return combinations
 
