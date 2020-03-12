a10a9feea440315b8400738d3b3d14d5064e8593
ellisrourkePC <ellisrourke@gmail.com>
2020-03-12 16:41:10 +1000

    still incoreect, adding solution that have less than the specified number of coins, but blocks larger solutions

--- recursion.py
+++ recursion.py
@@ -1,18 +1,20 @@
 import time
 class coinRecursion():
-    def sieve(self):
-        print("Calculating Sieve of Eratosthenes")
-        return [1,2,3,5,6,7,8,9]
+    def coins(self):
+        return [1,2,3,5,6,7,9]
 
     def __init__(self):
-        amount = 5
-        coins = self.sieve()
-        print(self.coinChange(amount,coins,0),"solutions found in",time.process_time(),"seconds")
+        amount = 8
+        coins = self.coins()
+        recusiveDepth = 3
+        goal_state = recusiveDepth
+        print(self.coinChange(amount,coins,0,recusiveDepth),"solutions found in",time.process_time(),"seconds")
 
-    def coinChange(self,amount,coins,currentCoin):
-        print(currentCoin,amount)
-
+    def coinChange(self,amount,coins,currentCoin,recursiveDepth):
+        if(recursiveDepth <= 0):
+            return 0
         if(amount == 0):
+            print("recursive depth",recursiveDepth," amount",amount)
             return 1
         if(amount < 0):
             return 0
@@ -20,10 +22,9 @@ class coinRecursion():
         combinations = 0
 
         for i in range(currentCoin,len(coins)):
-            combinations += self.coinChange(amount - coins[i],coins,i)
+         combinations += self.coinChange(amount - coins[i], coins, i, recursiveDepth - 1)
 
         return combinations
 
 solve = coinRecursion()
-#print(solve," solutions found - Time taken (secs) = ", time.process_time())
 
